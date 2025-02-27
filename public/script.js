8406abb25d97eec8e800cbe8c322441769790586
Rahitya26 <karamchedurahitya@gmail.com>
2025-02-27 14:51:53 +0530

    Push to git

---
+++ public/script.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function(){
+
+    $(".view").on("click",function(){
+        let btn = $(this);
+        let blog = btn.siblings(".content");
+        if(btn.text()=="View more"){
+            blog.text(blog.data("entire"));
+            btn.text("View less");
+        }
+        else{
+            let shorter = blog.data("entire").substring(0,400);
+            blog.text(shorter);
+            btn.text("View more");
+        }
+    });
+});
+
+
+$(".del").on("click",function(){
+    let btn = $(this);
+    let ind= btn.data("index");
+    $.post("/delete",{index:ind},function(){
+        location.reload();
+    });
+});
+
+
